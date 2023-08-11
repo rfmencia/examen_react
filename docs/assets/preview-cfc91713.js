b66925f229b4be23694cea6f256b88cff6d78eb5
rfmencia <rfmencia@gmail.com>
2023-08-11 14:24:41 -0400

    otro intento mas 3

---
+++ docs/assets/preview-cfc91713.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-f9b738b4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-56fddb58.js"),["./DocsRenderer-3PUGWF3O-56fddb58.js","./iframe-f9b738b4.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./react-18-ba2f8a3a.js","./index-6a5bd4ef.js","./index-8d4502de.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-cfc91713.js.map
